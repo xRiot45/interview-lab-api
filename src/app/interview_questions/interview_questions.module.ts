7f7a19925fe121252c24430c2eb7daa50cc1fd8c
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-01 13:56:26 +0700

    feat: create repository for interview question

--- src/app/interview_questions/interview_questions.module.ts
+++ src/app/interview_questions/interview_questions.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { InterviewQuestionsService } from './interview_questions.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InterviewQuestionEntity } from './entities/interview_question.entity';
 import { InterviewQuestionsController } from './interview_questions.controller';
+import { InterviewQuestionsService } from './interview_questions.service';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([InterviewQuestionEntity])],
     controllers: [InterviewQuestionsController],
     providers: [InterviewQuestionsService],
 })
